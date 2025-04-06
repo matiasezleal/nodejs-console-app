58fb72fd51665836e047ee1b8d4a04faae047be1
matiasezleal <matileal@yahoo.com>
2025-04-05 21:17:27 -0300

    App logic created

---
+++ src/app.logic.ts
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import {yarg} from "./config/plugins/args.plugin";
+
+
+let outputMessage = '';
+const base = yarg.b;
+const limit = yarg.l;
+const headerMEssage = `
+------------------------------------
+        Tabla del ${base}
+------------------------------------
+`;
+
+for(let i =1; i <= limit; i++) {
+    outputMessage += `${base} x ${i} = ${base * i }\n`;
+
+}
+
+if(yarg.s){
+    outputMessage = headerMEssage+ '\n' + outputMessage ;
+    console.log(outputMessage);
+}
+
+
+const outputPath = 'output/';
+
+fs.mkdirSync(outputPath, { recursive: true });
+fs.writeFileSync(`${outputPath}/table-${base}.txt`, outputMessage);
+
+console.log("File created!");
